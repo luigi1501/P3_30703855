fea1f54f5e88918d1c57be42b8ed44e11dff6376
Alealaes04 <alealaes04@gmail.com>
2023-12-02 13:45:44 -0400

    pagina usuario

--- db/models.js
+++ db/models.js
@@ -15,10 +15,15 @@ let querys = {
     updatecategory: 'UPDATE category SET name = ? WHERE id = ?',
     deleteproducto: 'DELETE FROM producto WHERE id = ?',
     deleteimagen: 'DELETE FROM imagen WHERE id = ?',
-    deletecategory: 'DELETE FROM category WHERE id = ?'
+    deletecategory: 'DELETE FROM category WHERE id = ?',
+    consultable: 'SELECT producto.id AS producto_id, producto.name AS producto_name, producto.price AS price, producto.description AS description, imagen.url AS imagen_url, category.name AS category_name FROM category INNER JOIN producto ON category.id = producto.category_id INNER JOIN imagen ON imagen.id = producto.id',
+    getdetalles: 'SELECT producto.id AS producto_id, producto.name AS producto_name, producto.code AS producto_code, producto.price AS price, producto.description AS description, category.name AS category_name, producto.brand AS brand, producto.model AS model, imagen.url AS imagen_url FROM producto INNER JOIN category ON category.id = producto.category_id INNER JOIN imagen ON imagen.id = producto.id'
 
 }
 module.exports = {
+
+
+
     getproducto(){
         return new Promise((resolve, reject)=>{
             db.all(querys.getproducto, (err,rows)=>{
@@ -156,5 +161,74 @@ module.exports = {
         })
     },
     
-}
 
+    consultable(producto_name, description, category_name, model, brand) {
+        return new Promise((resolve, reject) => {
+          let query = `
+            SELECT
+              producto.id AS producto_id,
+              producto.name AS producto_name,
+              producto.price AS price,
+              producto.description AS description,
+              producto.brand AS brand,
+              producto.model AS model,
+              imagen.url AS imagen_url,
+              category.name AS category_name
+            FROM category
+            INNER JOIN producto ON category.id = producto.category_id
+            INNER JOIN imagen ON imagen.id = producto.id
+          `;
+      
+          let whereClause = '';
+      
+          if (producto_name) {
+            whereClause += `producto.name LIKE '%${producto_name}%'`;
+          }
+      
+          if (description) {
+            if (whereClause.length > 0) {
+              whereClause += ` AND `;
+            }
+            whereClause += `producto.description LIKE '%${description}%'`;
+          }
+      
+          if (category_name) {
+            if (whereClause.length > 0) {
+              whereClause += ` AND `;
+            }
+            whereClause += `category.name = '${category_name}'`;
+          }
+      
+          if (model) {
+            if (whereClause.length > 0) {
+              whereClause += ` AND `;
+            }
+            whereClause += `producto.model LIKE '%${model}%'`;
+          }
+      
+          if (brand) {
+            if (whereClause.length > 0) {
+              whereClause += ` AND `;
+            }
+            whereClause += `producto.brand LIKE '%${brand}%'`;
+          }
+      
+          if (whereClause.length > 0) {
+            query += ` WHERE ${whereClause}`;
+          }
+      
+          db.all(query, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+          });
+        });
+      },
+    getdetalles(){
+        return new Promise((resolve, reject)=>{
+           db.all(querys.getdetalles ,(err, rows) => { 
+              if(err) reject(err);
+              resolve(rows);
+            });
+        });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,83 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/models');
 require('dotenv').config()
+const tablas = require ('../db/models')
 
 
 //login
 let logged = false;
 
+
+
+router.get('/filters', (req, res) => {
+  res.render('filters')
+  });
+  
+  router.get('/detalles/:id', (req, res) => {
+    db.getdetalles()
+      .then(data => {
+          res.render('detalles', {producto: data});
+      })
+      .catch(err => {
+        res.render('detalles', {producto: []});
+      });
+    });
+
+
+
 router.get('/', (req, res) => {
+  const { producto_name, description, category_name, brand, model } = req.query;
+  db.consultable(producto_name, description, category_name, brand, model)
+    .then(data => {
+      res.render('userpage', { producto: data });
+    })
+    .catch(err => {
+      console.error(err);
+      res.render('userpage', { producto: [] });
+    });
+});
+
+
+router.get('/login', (req,res) => {
   res.render('login')
-} )
+})
 
 router.post('/login', (req,res) =>{
   console.log(req.body)
   if (req.body.user === process.env.USER && req.body.pass === process.env.PASS) {
     console.log("Iniciaste")
     logged = true
+    tablas
     res.redirect('/administrar')
   
   } else {
     logged = false
-    res.redirect('/')
+    res.redirect('/login')
   }
+})
+
+router.get('/administrar', (req, res) =>{
+  tablas
+  res.render('administrar')
 })
 
 //index
 router.get('/index', (req, res) => {
+  tablas
   db.getproducto()
     .then(data => {        
       console.log(data)
+      tablas
       res.render('index', { producto: data });
   })
   .catch(err => {
       res.render('index', { producto: [] });
   })
-
-  
-
 });
 
 
-
 router.get('/tabcategory', (req, res) => {
+  tablas
   db.getcategory()
     .then(data => {        
       console.log(data)
@@ -54,6 +91,7 @@ router.get('/tabcategory', (req, res) => {
 });
 
 router.get('/tabimagen', (req, res) => {
+  tablas
   db.getimagen()
     .then(data => {        
       console.log(data)
@@ -67,22 +105,28 @@ router.get('/tabimagen', (req, res) => {
 
 
 router.get('/insertcat', (req, res) => {
+  tablas
   res.render('insertcat')
 } )
 
-router.get('/insertima', (req, res) => {
-  res.render('insertima')
-} )
 
 //insertar producto
 router.get('/insert', (req, res) => {
-  res.render('insert')
+  tablas
+  db.getcategory()
+  .then(data => {
+    res.render('insert', {category: data});
+  })
+  .catch(err => {
+    res.render('insert', {category: []});
+  })
 } )
 
 
 router.post('/insert', (req, res) => {
   const {code, name, brand, model, description, price, category_id} = req.body;
   console.log(code, name, brand, model, description, price, category_id);
+  tablas
   db.insertproducto(code, name,brand,model,description,price,category_id)
   .then(() => {
      res.redirect('index')
@@ -95,6 +139,7 @@ router.post('/insert', (req, res) => {
 router.post('/insertcat', (req, res) => {
   const {name} = req.body;
   console.log(name);
+  tablas
   db.insertcategory(name)
   .then(() => {
      res.redirect('tabcategory')
@@ -107,6 +152,7 @@ router.post('/insertcat', (req, res) => {
 router.post('/insertima', (req, res) => {
   const {url, producto_id, destacado} = req.body;
   console.log(url, producto_id, destacado);
+  tablas
   db.insertimagen(url, producto_id, destacado)
   .then(() => {
      res.redirect('tabimagen')
@@ -117,13 +163,14 @@ router.post('/insertima', (req, res) => {
 });
 
 
-
 //editar producto
 router.post('/edit/', (req, res)=>{
   const {id, code, name, brand, model, description, price, category_id,} = req.body;
+  tablas
   db.updateproducto(id, code, name, brand, model, description, price, category_id)
   .then(() =>{
     res.redirect('/index');
+    console.log(id, code, name, brand, model, description, price, category_id);
   })
   .catch(err =>{
     console.log(err);
@@ -133,6 +180,7 @@ router.post('/edit/', (req, res)=>{
 
 router.post('/editcat/', (req, res)=>{
   const {id, name} = req.body;
+  tablas
   db.updatecategory(id, name)
   .then(() =>{
     res.redirect('/tabcategory');
@@ -145,11 +193,15 @@ router.post('/editcat/', (req, res)=>{
 
 
 
+
+
 router.post('/editima/', (req, res)=>{
   const {id, url, producto_id, destacado} = req.body;
+  tablas
   db.updateimagen(id, url, producto_id, destacado)
   .then(() =>{
     res.redirect('/tabimagen');
+    console.log(id, url, producto_id, destacado);
   })
   .catch(err =>{
     console.log(err);
@@ -161,6 +213,7 @@ router.post('/editima/', (req, res)=>{
 
 router.get('/edit/:id', (req, res)=>{
   const id = req.params.id
+  tablas
   db.getproductoID(id)
   .then(data =>{
     console.log(data)
@@ -176,9 +229,11 @@ router.get('/edit/:id', (req, res)=>{
 })
 
 router.get('/editima/:id', (req, res)=>{
+  tablas
   const id = req.params.id
   db.getimagenID(id)
   .then(data =>{
+    db.getimagen
     console.log(data)
     res.render('editima', {imagen: data[0]})
   })
@@ -193,6 +248,7 @@ router.get('/editima/:id', (req, res)=>{
 
 router.get('/editcat/:id', (req, res)=>{
   const id = req.params.id
+  tablas
   db.getcategoryID(id)
   .then(data =>{
     console.log(data)
@@ -209,6 +265,7 @@ router.get('/editcat/:id', (req, res)=>{
 
 router.get('/delete/:id', (req, res)=>{
   const id = req.params.id;
+  tablas
   db.deleteproducto(id)
     .then(() => {
     res.redirect('/index');
@@ -218,15 +275,15 @@ router.get('/delete/:id', (req, res)=>{
   });
 })
 
-router.get('/administrar', (req, res) =>{
-  res.render('administrar')
-})
+
 
 router.get('/tabcategory', (req, res) =>{
+  tablas
   res.render('tabcategory')
 })
 
 router.get('/deletecat/:id', (req, res)=>{
+  tablas
   const id = req.params.id;
   db.deletecategory(id)
     .then(() => {
@@ -237,8 +294,14 @@ router.get('/deletecat/:id', (req, res)=>{
   });
 })
 
+router.get('/tabcategory', (req, res) =>{
+  tablas
+  res.render('tabcategory')
+})
+
 router.get('/deleteima/:id', (req, res)=>{
   const id = req.params.id;
+  tablas
   db.deleteimagen(id)
     .then(() => {
     res.redirect('/tabimagen');
@@ -248,7 +311,16 @@ router.get('/deleteima/:id', (req, res)=>{
   });
 })
 
-
+router.get('/insertima', (req, res) => {
+  tablas
+  db.getproducto()
+  .then(data => {
+    res.render('insertima', {producto: data});
+  })
+  .catch(err => {
+    res.render('insertima', {producto: []});
+  })
+});
 
 
 module.exports = router;

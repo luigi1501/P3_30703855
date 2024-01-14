d157f8ec80c25c4685c0c57cd7cbcc496798b0a4
Alealaes04 <alealaes04@gmail.com>
2024-01-13 20:57:34 -0400

    actualizacion

--- db/models.js
+++ db/models.js
@@ -1,6 +1,8 @@
 const db = require('./connection');
 
 let querys = {
+    getuser:'SELECT * FROM usuarios',
+    register:'INSERT INTO usuarios(name, email, password) VALUES(?, ?, ?)',
     getproducto: 'SELECT * FROM producto',
     getproductoID: 'SELECT * FROM producto WHERE id = ?',
     getimagenID: 'SELECT * FROM imagen WHERE id = ?',
@@ -17,12 +19,62 @@ let querys = {
     deleteimagen: 'DELETE FROM imagen WHERE id = ?',
     deletecategory: 'DELETE FROM category WHERE id = ?',
     consultable: 'SELECT producto.id AS producto_id, producto.name AS producto_name, producto.price AS price, producto.description AS description, imagen.url AS imagen_url, category.name AS category_name FROM category INNER JOIN producto ON category.id = producto.category_id INNER JOIN imagen ON imagen.id = producto.id',
-    getdetalles: 'SELECT producto.id AS producto_id, producto.name AS producto_name, producto.code AS producto_code, producto.price AS price, producto.description AS description, category.name AS category_name, producto.brand AS brand, producto.model AS model, imagen.url AS imagen_url FROM producto INNER JOIN category ON category.id = producto.category_id INNER JOIN imagen ON imagen.id = producto.id'
-
+    getdetalles: 'SELECT producto.id AS producto_id, producto.name AS producto_name, producto.code AS producto_code, producto.price AS price, producto.description AS description, category.name AS category_name, producto.brand AS brand, producto.model AS model, imagen.url AS imagen_url, imagen.id AS imagen_id FROM producto INNER JOIN category ON category.id = producto.category_id INNER JOIN imagen ON imagen.id = producto.id',
+    getcompra: 'SELECT * FROM compras'
+    
 }
 module.exports = {
 
 
+
+
+    facturas(cantidad, total_pagado, fecha, ip_cliente, transaccion_id, descripcion, referencia, moneda_id, cliente_id, producto_id){
+        return new Promise ((resolve, reject)=>{
+            const sql= 'INSERT INTO compras (cantidad, total_pagado, fecha, ip_cliente, transaccion_id, descripcion, referencia, moneda_id, cliente_id, producto_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            db.run(sql, [cantidad, total_pagado, fecha, ip_cliente, transaccion_id, descripcion, referencia, moneda_id, cliente_id, producto_id], (err, resultados)=>{
+                if (err) reject(err);
+                else resolve (resultados);
+            });
+        })
+    },
+
+    insertcompra(cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente){
+        return new Promise((resolve, reject) => {
+            db.run(querys.insertcompra, [cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
+    
+    },
+
+    getcompra(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getcompra, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+    getuser(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getuser, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+
+    
+    },
+
+    register(name, email, password){
+        return new Promise((resolve, reject) => {
+            db.run(querys.register, [name, email, password], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
+    },
 
     getproducto(){
         return new Promise((resolve, reject)=>{
@@ -232,3 +284,8 @@ module.exports = {
         });
     }
 }
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,237 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/models');
 require('dotenv').config()
+var axios = require ('axios');
+const { Axios } = require('axios');
 const tablas = require ('../db/models')
 
 
 //login
 let logged = false;
+let logeedin = false;
 
 
+//Pago productos API 
+router.post('/payments', async (req, res, next)=>{
+  var monto, moneda;
+  const {descripcion, nombre, numero_tarjeta, cvv, mes_ven, year_ven, moneda_id, cantidad, referencia, precio} = req.body;
+  const ip_cliente = req.socket.remoteAddress;
+  if (moneda_id == 1) {
+    moneda= 'USD';
+    monto = cantidad * precio;
+  }else{
+    if (moneda_id == 2) {
+      moneda= 'EUR';
+      monto = (cantidad * precio) * 0.91;
+    } else {
+      if (moneda_id == 3) {
+        moneda= 'VES';
+        monto = (cantidad * precio) * 35.94; 
+      }
+    }
+  }
+  const payments ={
+    "amount": monto,
+    "card-number": numero_tarjeta,
+    "cvv": cvv,
+    "expiration-month": mes_ven,
+    "expiration-year": year_ven,
+    "full-name": nombre,
+    "currency": moneda,
+    "description": descripcion,
+    "reference": referencia
+  }
+  try{
+    const response = await axios.post ('https://fakepayment.onrender.com/payments', payments, {headers:{ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJkYXRlIjoiMjAyNC0wMS0xMVQyMjoxMzoyMC41NTZaIiwiaWF0IjoxNzA1MDExMjAwfQ.MC2WfwWwfyRT4Q6q9-D1n73rrorClhC1Ih4Lb0o1_sI'}});
+    const data = JSON.parse(JSON.stringify(response.data));
+      const transaccion_id = data.data.transaction_id;
+      const total_pagado = data.data.amount;
+      const fecha = data.data.date;
+      const referencia = data.data.reference;
+      const descripcion = data.data.description;
+      const message = data.message;
+      console.log(message);
+      productosModel
+      .facturas(cantidad, total_pagado, fecha, ip_cliente, transaccion_id, descripcion, referencia, moneda_id, cliente_id, producto_id)
+      .then(idFacturaRealizada =>{
+        res.render('pagosuccess', {title: 'Compra Exitosa'})
+      })
+      
+  } catch (err) {
+    res.render('pagofails');
+  }
+})
 
-router.get('/filters', (req, res) => {
-  res.render('filters')
+// compra
+router.get('/compra', (req, res) => {
+  db.getproducto()
+  .then(data =>{
+    db.getuser()
+    .then (client => { 
+      console.log(client);
+      res.render ('compra', { producto: data, cliente: client });
+    })
+    .catch (err => {
+      res.render ('compra', { producto: data, cliente: client  });
+    })
+  })     
+  .catch (err => {
+    res.render ('compra', { producto: [], cliente: [] });
   });
-  
-  router.get('/detalles/:id', (req, res) => {
-    db.getdetalles()
+} )
+
+//Pagina formulario de compra 
+router.get('/pedidoprd/:id', function(req, res, next){
+  if(req.session.auth){
+    const id = req.params.id
+    productosModel
+      .obtenerPorId(id)
+      .then(datos=>{
+        res.render('pedidoprd', {datos: datos});
+      })
+      .catch(err=>{
+        console.error(err.message);
+        return res.status(500).send('No se encuentra el producto')
+      })
+  } else{
+    res.redirect('/userini');
+  }
+})
+
+
+
+
+router.post('/compra', function(req, res, next) {
+  let date = new Date();
+  let Datetime = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+  let fecha = Datetime;
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const ip_cliente = ip.split(",")[0];
+  const cliente_id = req.body.cliente_id
+  const producto_id = req.body.producto_id
+  const cantidad = req.body.cantidad
+  const bd = require('../db/connection');
+  let sql = `SELECT price FROM producto WHERE id = ?`;
+  let precio;
+ 
+  bd.get(sql, [producto_id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    precio = row.price;
+    console.log(`El precio del producto ${producto_id} es: ${precio}`);
+   
+    let total_pagado = precio * cantidad;
+    console.log(`El resultado de la multiplicación es: ${total_pagado}`);
+
+    db.insertcompra(cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente)
+    .then(() => {
+       res.redirect('payments')
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+  });
+ 
+})
+
+
+router.get('/tabcliente', (req, res) => {
+  db.getuser()
+    .then(data => {        
+      console.log(data)
+      res.render('tabcliente', { usuarios: data });
+  })
+  .catch(err => {
+      res.render('tabcliente', { usuarios: [] });
+  })
+
+});
+
+router.get('/payments', (req, res) => {
+  res.render('payments')
+  });
+
+  router.get('/paga', (req, res) => {
+    res.render('paga')
+    });
+
+
+
+
+  router.get('/filterslog', (req, res) => {
+    const { producto_name, description, category_name, brand, model } = req.query;
+    db.consultable(producto_name, description, category_name, brand, model)
       .then(data => {
-          res.render('detalles', {producto: data});
+        res.render('pageini', { producto: data });
       })
       .catch(err => {
-        res.render('detalles', {producto: []});
+        console.error(err);
+        res.render('pageini', { producto: [] });
       });
-    });
+  });
+  
+
+
+
+    router.get('/filters', (req, res) => {
+      res.render('filters')
+      });
+      
+      router.get('/imagen/:id', (req, res)=>{
+        const id = req.params.id
+        db.getimagenID(id)
+        .then(data =>{
+          console.log(data)
+          res.render('imagen', {imagen: data[0]})
+        })
+          .catch(err =>{
+            console.log(err);
+            res.render('imagen', {imagen: []})
+          })   
+      })
+
+
+
+      router.get('/detallesini/:id', (req, res)=>{
+        const id = req.params.id
+        db.getproductoID(id)
+        .then(data =>{
+          console.log(data)
+          db.getimagen()
+          .then (images => { 
+            res.render ('detallesini', { producto: data[0], imagen: images });
+          })
+          .catch (err => {
+            res.render ('detallesini', { producto: data[0], imagen: [] });
+          })
+        })     
+        .catch (err => {
+          res.render ('detallesini', { producto: [], imagen: [] });
+        });
+      })
+
+
+
+
+      router.get('/detalles/:id', (req, res)=>{
+        const id = req.params.id
+        db.getproductoID(id)
+        .then(data =>{
+          console.log(data)
+          db.getimagen()
+          .then (images => { 
+            res.render ('detalles', { producto: data[0], imagen: images });
+          })
+          .catch (err => {
+            res.render ('detalles', { producto: data[0], imagen: [] });
+          })
+        })     
+        .catch (err => {
+          res.render ('detalles', { producto: [], imagen: [] });
+        });
+      })
 
 
 
@@ -39,6 +249,61 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/pageini', (req, res) => {
+  const { producto_name, description, category_name, brand, model } = req.query;
+  db.consultable(producto_name, description, category_name, brand, model)
+    .then(data => {
+      res.render('pageini', { producto: data });
+    })
+    .catch(err => {
+      console.error(err);
+      res.render('pageini', { producto: [] });
+    });
+});
+
+router.get('/register', (req,res)=> {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const {name,email, password} = req.body;
+  console.log(name, email, password);
+  db.register(name, email, password)
+  .then(() => {
+     res.redirect('pageini')
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});  
+
+router.get('/userini', (req,res)=> {
+  res.render('userini')
+})
+
+router.post('/pageini', (req, res) => {
+  const email = req.body.email; 
+  const password = req.body.password;
+  console.log(email, password);
+  const bd = require('../db/connection');
+  bd.get('SELECT * FROM usuarios WHERE email = ? AND password = ?', [email, password], (err, row) => {
+    if (err) {
+      console.error(err);
+      // Manejar el error
+    } else {
+      if (row) {
+        console.log(" Los datos son iguales, redirigir a otra vista")
+        res.redirect('/pageini');
+      } else {
+        console.log(" Los datos no son iguales, manejar según sea necesario")
+        res.redirect('/userini');
+      }
+    }
+  });
+});
+
+
+
 router.get('/login', (req,res) => {
   res.render('login')
 })
@@ -48,7 +313,6 @@ router.post('/login', (req,res) =>{
   if (req.body.user === process.env.USER && req.body.pass === process.env.PASS) {
     console.log("Iniciaste")
     logged = true
-    tablas
     res.redirect('/administrar')
   
   } else {
@@ -58,7 +322,6 @@ router.post('/login', (req,res) =>{
 })
 
 router.get('/administrar', (req, res) =>{
-  tablas
   res.render('administrar')
 })
 
@@ -321,6 +584,9 @@ router.get('/insertima', (req, res) => {
     res.render('insertima', {producto: []});
   })
 });
+
+
+
 
 
 module.exports = router;
